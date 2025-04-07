667079875685400d8023230a835b09e4705bfae6
Dennis Elsinga <personal.elsinga@gmail.com>
2025-04-07 20:41:58 +0200

    Add HasFactory trait to DocumentationLink model

--- app/Models/DocumentationLink.php
+++ app/Models/DocumentationLink.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Enums\Framework;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class DocumentationLink extends Model
 {
+    /** @use HasFactory<DocumentationLinkFactory> */
+    use HasFactory;
+
     protected function casts(): array
     {
         return [
