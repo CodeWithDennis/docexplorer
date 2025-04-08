0baa0f4ba50e703d2171e305ee3e186dae33a8bf
Dennis Elsinga <personal.elsinga@gmail.com>
2025-04-08 20:19:09 +0200

    Add FilamentPHP and Livewire documentation links

--- database/seeders/DocumentationLinkSeeder.php
+++ database/seeders/DocumentationLinkSeeder.php
@@ -21,6 +21,24 @@ final class DocumentationLinkSeeder extends Seeder
                 'category' => $link['category'],
             ]);
         }
+
+        foreach ($this->getFilamentPHPLinks() as $link) {
+            DocumentationLink::create([
+                'title' => $link['title'],
+                'url' => $link['url'],
+                'framework' => Framework::Filament,
+                'category' => $link['category'],
+            ]);
+        }
+
+        foreach ($this->getLivewireLinks() as $link) {
+            DocumentationLink::create([
+                'title' => $link['title'],
+                'url' => $link['url'],
+                'framework' => Framework::Livewire,
+                'category' => $link['category'],
+            ]);
+        }
     }
 
     /**
@@ -29,6 +47,58 @@ final class DocumentationLinkSeeder extends Seeder
      * @return array<int, array<string, string>>
      */
     private function getLaravelLinks(): array
+    {
+        return [
+            [
+                'url' => 'https://laravel.com/docs/12.x/installation',
+                'title' => 'Example A',
+                'category' => 'Examples',
+            ],
+            [
+                'url' => 'https://laravel.com/docs/12.x/installation',
+                'title' => 'Example B',
+                'category' => 'Examples',
+            ],
+            [
+                'url' => 'https://laravel.com/docs/12.x/installation',
+                'title' => 'Example C',
+                'category' => 'Examples',
+            ],
+        ];
+    }
+
+    /**
+     * Get FilamentPHP documentation links.
+     *
+     * @return array<int, array<string, string>>
+     */
+    private function getFilamentPHPLinks(): array
+    {
+        return [
+            [
+                'url' => 'https://filamentphp.com/docs/1.x/installation',
+                'title' => 'Example A',
+                'category' => 'Examples',
+            ],
+            [
+                'url' => 'https://filamentphp.com/docs/1.x/installation',
+                'title' => 'Example B',
+                'category' => 'Examples',
+            ],
+            [
+                'url' => 'https://filamentphp.com/docs/1.x/installation',
+                'title' => 'Example C',
+                'category' => 'Examples',
+            ],
+        ];
+    }
+
+    /**
+     * Get Livewire documentation links.
+     *
+     * @return array<int, array<string, string>>
+     */
+    private function getLivewireLinks(): array
     {
         return [
             [
