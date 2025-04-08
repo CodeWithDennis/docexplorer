9ef9d8163d2a8e28ee048666035b6abef915ce57
Dennis Elsinga <personal.elsinga@gmail.com>
2025-04-08 20:02:55 +0200

    Add high score tracking functionality

--- app/Livewire/DocumentationLinks/Random.php
+++ app/Livewire/DocumentationLinks/Random.php
@@ -11,9 +11,29 @@ final class Random extends Component
 {
     public ?DocumentationLink $link = null;
 
+    public int $streak = 0;
+
+    public int $highScore = 0;
+
+    public function mount(): void
+    {
+        $this->highScore = session('high_score', 0);
+    }
+
     public function getRandomLink(): void
     {
         $this->link = DocumentationLink::query()->inRandomOrder()->first();
+
+        if ($this->link->exists()) {
+            $this->link->increment('discoveries_count');
+            $this->streak++;
+
+            if ($this->streak > $this->highScore) {
+                $this->highScore = $this->streak;
+
+                session(['high_score' => $this->highScore]);
+            }
+        }
     }
 
     public function render()
