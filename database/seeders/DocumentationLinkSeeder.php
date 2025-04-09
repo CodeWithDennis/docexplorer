5a2314e62a7c76dd91f5ccf25dd645c830714a92
Dennis Elsinga <personal.elsinga@gmail.com>
2025-04-09 15:52:09 +0200

    Add new Filament documentation links to the seeder

--- database/seeders/DocumentationLinkSeeder.php
+++ database/seeders/DocumentationLinkSeeder.php
@@ -4,22 +4,26 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Enums\Framework;
 use App\Models\DocumentationLink;
 use Illuminate\Database\Seeder;
 
 final class DocumentationLinkSeeder extends Seeder
 {
-    public function laravel(): void
-    {
-        $laravelDocs = json_decode(file_get_contents(database_path('seeders/data/laravel-docs.json')), true);
-
-        foreach ($laravelDocs as $doc) {
-            DocumentationLink::create($doc);
-        }
-    }
-
     public function run(): void
     {
-        $this->laravel();
+        $frameworks = Framework::cases();
+
+        foreach ($frameworks as $framework) {
+            if (! file_exists(database_path('seeders/data/' . $framework->value . '-docs.json'))) {
+                continue;
+            }
+
+            $laravelDocs = json_decode(file_get_contents(database_path('seeders/data/' . $framework->value . '-docs.json')), true);
+
+            foreach ($laravelDocs as $doc) {
+                DocumentationLink::create($doc);
+            }
+        }
     }
 }
